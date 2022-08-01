aa0542053b898515fac9c1ecb3aae368942a142e
Hemanth Gokavarapu <hemanth@soluble.ai>
2022-08-01 13:55:24 +0530

    test PR
    Signed-off-by: Hemanth Gokavarapu <hemanth@soluble.ai>

--- terraform/gcp/kubernetes.tf
+++ terraform/gcp/kubernetes.tf
@@ -18,7 +18,6 @@ resource "google_container_cluster" "primary" {
   node_config {
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
     ]
   }
